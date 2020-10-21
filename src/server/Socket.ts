2a22288ae9c82964a6fdf602794d4c2415ba3da0
Fraser McCallum <fraser@themccallums.net.nz>
2020-10-22 03:11:10 +1300

    Implemented authentication requirements. Socketio events are not listened for until authentication is acquired.

--- src/server/Socket.ts
+++ src/server/Socket.ts
@@ -1,81 +1,102 @@
 import { inject, injectable } from "inversify";
 import * as io from "socket.io";
-import { createServer, Server } from "http";
 import { TYPES } from "../bindings/types";
 import { Log } from "../helpers/Log";
 import { GameRegister } from "../model/GameRegister";
 import { GameSettings } from "../model/Game";
+import { ClientModel } from "./database/models/client/Client.Model";
+import { Server } from "http";
+
+const socketioAuth = require("socketio-auth");
 
 @injectable()
 class SocketManagerImpl {
-    public static readonly PORT = 7373;
-
     private ioServer: io.Server;
 
     constructor(
         @inject(TYPES.Log) private readonly log: Log,
-        @inject(TYPES.GameRegister) private readonly gameRegister: GameRegister
+        @inject(TYPES.GameRegister) private readonly gameRegister: GameRegister,
+        @inject(TYPES.Server) private readonly server: Server
     ) {
-        const server: Server = createServer();
-        this.ioServer = io.default(server);
+        this.ioServer = io.default(this.server);
 
-        this.ioServer.listen(SocketManagerImpl.PORT);
+        socketioAuth(this.ioServer, { authenticate: this.authenticate, postAuthenticate: this.postAuthenticate });
 
         this.ioServer.on("connection", (client) => {
-
             log.trace("SocketManagerImpl", "Client connected to server");
-
-            client.on("disconnect", () => {
-                log.trace("SocketManagerImpl", "Client disconnected from server.");
-            });
-
-            client.on("hostGameRequest", (name: string = "", gameSettings: GameSettings = {}) => {
-                const game = gameRegister.generateGame();
-                game.setGameSettings(gameSettings);
-
-                (client as EmittableEvents).emit("gameHostGiven", game.Code);
-                client.on("clientReady", () => {
-                    game.addHostPlayer(name, client);
-                });
-
-                log.trace("SocketManagerImpl", `New Game Initiated by ${name} with code ${game.Code}`);
-            });
-
-            client.on("joinGameRequest", (name: string, id: string) => {
-                const game = gameRegister.findGame(id);
-
-                if (game === undefined) {
-                    client.emit("joinGameNotification", false, "Game was not found.");
-                    return log.trace("SocketManagerImpl", `${name} tried to join game with code ${id}.`);
-                }
-
-                if (game.isFull()) {
-                    client.emit("joinGameNotification", false, "Game is full.");
-                    return log.trace("SocketManagerImpl", `${name} tried to join game with code ${id}.`);
-                }
-
-                if (game.getPlayerNames().includes(name)) {
-                    client.emit("joinGameNotification", false, "Someone already has that name.");
-                    return log.trace("SocketManagerImpl", `${name} tried to join game with code ${id}.`);
-                }
-
-                client.emit("joinGameNotification", true, game.getPlayerNames());
-                client.once("clientReady", () => {
-                    game.addPlayer(name, client);
-                    log.trace("SocketManagerImpl", `${name} joined game joined with code ${id}.`);
-                });
-            });
-
-            client.on("browseGames", () => {
-                const games = gameRegister.getPublicGames();
-                const transmitData = games.map((g) => g.getGameInfo());
-                client.emit("browseGameDataLoaded", transmitData);
-            });
-
         });
     }
 
-    private listenToClient() {}
+    private postAuthenticate = (client: ioClientUsername) => {
+        this.log.trace("SocketManagerImpl", `${client.username} authenticated with server`);
+
+        client.emit("ready");
+
+        client.on("disconnect", () => {
+            this.log.trace("SocketManagerImpl", "Client disconnected from server.");
+        });
+
+        client.on("hostGameRequest", (name: string = "", gameSettings: GameSettings = {}) => {
+            const game = this.gameRegister.generateGame();
+            game.setGameSettings(gameSettings);
+
+            (client as EmittableEvents).emit("gameHostGiven", game.Code);
+            client.on("clientReady", () => {
+                game.addHostPlayer(name, client);
+            });
+
+            this.log.trace("SocketManagerImpl", `New Game Initiated by ${name} with code ${game.Code}`);
+        });
+
+        client.on("joinGameRequest", (name: string, id: string) => {
+            const game = this.gameRegister.findGame(id);
+
+            if (game === undefined) {
+                client.emit("joinGameNotification", false, "Game was not found.");
+                return this.log.trace("SocketManagerImpl", `${name} tried to join game with code ${id}.`);
+            }
+
+            if (game.isFull()) {
+                client.emit("joinGameNotification", false, "Game is full.");
+                return this.log.trace("SocketManagerImpl", `${name} tried to join game with code ${id}.`);
+            }
+
+            if (game.getPlayerNames().includes(name)) {
+                client.emit("joinGameNotification", false, "Someone already has that name.");
+                return this.log.trace("SocketManagerImpl", `${name} tried to join game with code ${id}.`);
+            }
+
+            client.emit("joinGameNotification", true, game.getPlayerNames());
+            client.once("clientReady", () => {
+                game.addPlayer(name, client);
+                this.log.trace("SocketManagerImpl", `${name} joined game joined with code ${id}.`);
+            });
+        });
+
+        client.on("browseGames", () => {
+            const games = this.gameRegister.getPublicGames();
+            const transmitData = games.map((g) => g.getGameInfo());
+            client.emit("browseGameDataLoaded", transmitData);
+        });
+    };
+
+    private authenticate = async (
+        client: ioClientUsername,
+        data: { username: string; password: string },
+        callback: (...args: any) => void
+    ) => {
+        const { username, password } = data;
+        const foundUser = await ClientModel.findOne({
+            username: { $regex: new RegExp("^" + username.trim() + "$", "i") },
+        });
+        if (!foundUser) return callback(new Error("User Doesn't Exist"));
+
+        const result = await foundUser.verifyPassword(password);
+        if (result) client.emit("username", foundUser.username);
+        client.username = foundUser.username;
+
+        return callback(null, result);
+    };
 }
 
 interface SocketManager {}
@@ -84,4 +105,8 @@ export { SocketManager, SocketManagerImpl };
 
 declare interface EmittableEvents extends io.Socket {
     emit(event: "gameHostGiven", code: string): boolean;
+}
+
+interface ioClientUsername extends io.Socket {
+    username: string;
 }
