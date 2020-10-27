b6bc3dd399cd1a9443d57399c7fd42c98c95240d
Fraser McCallum <fraser@themccallums.net.nz>
2020-10-28 02:43:05 +1300

    Fixed bugs and added stat tracking

--- src/model/Player.ts
+++ src/model/Player.ts
@@ -1,33 +1,39 @@
 import { Socket } from "socket.io";
+import { IClientDocument } from "../server/database/models/client/Client.Model";
 import { Question, SendableQuestionData } from "./Questions/Question";
 
 class PlayerImpl implements Player {
     private points: number = 0;
+    private mostRecentPoints: number = 0;
+    private pointsArray: number[] = [];
 
     constructor(private readonly name: string, private readonly client: Socket) {}
 
-    signalGameOver(winners: PlayerSummary[]): void {
-        this.client.emit("gameFinished", winners);
+    signalGameOver(winners: PlayerSummary[], yourPoints: number): void {
+        this.client.emit("gameFinished", winners, yourPoints);
     }
 
     signalRoundOver(solution: string, playerPoints: number, topPlayers: PlayerSummary[]): void {
-        this.client.emit("roundOver", solution, playerPoints, topPlayers);
+        this.client.emit("roundOver", solution, playerPoints, topPlayers, this.mostRecentPoints);
+        this.mostRecentPoints = 0;
     }
 
-    signalPlayerCountChange(players: string[], max: number): void {
-        this.client.emit("playersChange", players, max);
+    signalPlayerCountChange(players: string[], xp: number[], max: number): void {
+        this.client.emit("playersChange", players, xp, max);
     }
 
     signalNewQuestion(question: SendableQuestionData): void {
         this.client.emit("newQuestion", question.question, question.prompt);
     }
 
-    signalGameStart(): void {
-        this.client.emit("gameStart");
+    signalGameStart(timer: number): void {
+        this.client.emit("gameStart", timer);
     }
 
-    signalCorrectnessOfAnswer(correct: boolean): void {
+    signalCorrectnessOfAnswer(correct: boolean, points: number): void {
         this.client.emit("answerResult", correct);
+        this.mostRecentPoints = points;
+        this.pointsArray.push(points);
     }
 
     signalGameInterrupt(): void {
@@ -38,6 +44,18 @@ class PlayerImpl implements Player {
         this.client.emit("kicked");
     }
 
+    getSocket() {
+        return this.client;
+    }
+
+    increasePoints(pointsToAdd: number): void {
+        this.points += pointsToAdd;
+    }
+
+    setMostRecentPoints(n: number): void {
+        this.mostRecentPoints = n;
+    }
+
     get Name(): string {
         return this.name;
     }
@@ -46,12 +64,8 @@ class PlayerImpl implements Player {
         return this.points;
     }
 
-    getSocket() {
-        return this.client;
-    }
-
-    increasePoints(pointsToAdd: number): void {
-        this.points += pointsToAdd;
+    playerGameStats(): [correct: number, incorrect: number] {
+        return [this.pointsArray.filter((p) => !p).length, this.pointsArray.filter((p) => p).length];
     }
 }
 
@@ -66,19 +80,23 @@ interface Player extends PlayerSummary {
 
     signalRoundOver(solution: string, playerPoints: number, topPlayers: PlayerSummary[]): void;
 
-    signalGameOver(winners: PlayerSummary[]): void;
+    signalGameOver(winners: PlayerSummary[], yourPoints: number): void;
 
-    signalPlayerCountChange(players: string[], max: number): void;
+    signalPlayerCountChange(players: string[], xp: number[], max: number): void;
 
-    signalCorrectnessOfAnswer(correct: boolean): void;
+    signalCorrectnessOfAnswer(correct: boolean, points: number): void;
 
-    signalGameStart(): void;
+    signalGameStart(timer: number): void;
 
     signalGameInterrupt(): void;
 
     getSocket(): Socket;
 
     increasePoints(pointsToAdd: number): void;
+
+    setMostRecentPoints(n: number): void;
+
+    playerGameStats(): [correct: number, incorrect: number]
 }
 
 interface PlayerSummary {
