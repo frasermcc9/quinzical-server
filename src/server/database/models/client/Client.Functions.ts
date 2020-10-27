914cb3c9630ae1704f31506ffd1a8bacdc0a0b59
Fraser McCallum <fraser@themccallums.net.nz>
2020-10-28 02:41:44 +1300

    Added more stats to database model

--- src/server/database/models/client/Client.Functions.ts
+++ src/server/database/models/client/Client.Functions.ts
@@ -1,4 +1,4 @@
-import { IClientDocument, IClientModel } from "./Client.Model";
+import { ClientModel, IClientDocument, IClientModel } from "./Client.Model";
 
 import { hash, compare } from "bcrypt";
 import { HookNextFunction } from "mongoose";
@@ -25,13 +25,58 @@ export async function verifyPassword(this: IClientDocument, given: string): Prom
     return result;
 }
 
+export function getXp(this: IClientDocument): number {
+    return this?.stats?.XP ?? 0;
+}
+
+export async function addXp(this: IClientDocument, amount: number): Promise<void> {
+    verifyData(this);
+    this.stats!.XP = (this.stats?.XP ?? 0) + amount;
+    await this.save();
+}
+
+export async function addCorrect(this: IClientDocument, amount: number): Promise<void> {
+    verifyData(this);
+    this.stats!.correct = (this.stats?.correct ?? 0) + amount;
+}
+
+export async function addIncorrect(this: IClientDocument, amount: number): Promise<void> {
+    verifyData(this);
+    this.stats!.incorrect = (this.stats?.incorrect ?? 0) + amount;
+}
+
+function verifyData(document: IClientDocument) {
+    if (document.stats == undefined) {
+        document.stats = { XP: 0, correct: 0, incorrect: 0 };
+    }
+    if (document.stats.XP == undefined) {
+        document.stats.XP = 0;
+    }
+    if (document.stats.correct == undefined) {
+        document.stats.correct = 0;
+    }
+    if (document.stats.incorrect == undefined) {
+        document.stats.incorrect = 0;
+    }
+}
+
 //#endregion
 
 //#region MODEL METHODS
 
 export async function createNewUser(this: IClientModel, username: string, password: string): Promise<IClientDocument> {
-    const newClient = this.create({ username: username, password: password });
+    const newClient = this.create({
+        username: username,
+        password: password,
+        stats: { XP: 0, correct: 0, incorrect: 0 },
+    });
     return newClient;
+}
+
+export async function findPlayer(this: IClientModel, name: string): Promise<IClientDocument | null> {
+    return await ClientModel.findOne({
+        username: { $regex: new RegExp("^" + name.trim() + "$", "i") },
+    });
 }
 
 //#endregion
--- src/server/database/models/client/Client.Schema.ts
+++ src/server/database/models/client/Client.Schema.ts
@@ -1,9 +1,25 @@
 import { Schema } from "mongoose";
-import { createNewUser, saveAndUpdate, verifyPassword } from "./Client.Functions";
+import {
+    addCorrect,
+    addIncorrect,
+    addXp,
+    createNewUser,
+    findPlayer,
+    getXp,
+    saveAndUpdate,
+    verifyPassword,
+} from "./Client.Functions";
 
 const ClientSchema = new Schema({
     username: { type: String, required: true },
     password: { type: String, required: true },
+
+    stats: {
+        XP: { type: Number },
+        correct: { type: Number },
+        incorrect: { type: Number },
+    },
+
     dateOfEntry: { type: Date, default: new Date() },
     lastUpdated: { type: Date, default: new Date() },
 });
@@ -11,7 +27,13 @@ const ClientSchema = new Schema({
 ClientSchema.methods.saveAndUpdate = saveAndUpdate;
 ClientSchema.methods.verifyPassword = verifyPassword;
 
+ClientSchema.methods.addXp = addXp;
+ClientSchema.methods.getXp = getXp;
+ClientSchema.methods.addCorrect = addCorrect;
+ClientSchema.methods.addIncorrect = addIncorrect;
+
 ClientSchema.statics.createNewUser = createNewUser;
+ClientSchema.statics.findPlayer = findPlayer;
 
 ClientSchema.pre("save", saveAndUpdate);
 
